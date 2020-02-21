f29fd7691ecaafcec1e843703788561d44aeecc8
Fahad889 <55811140+Fahad889@users.noreply.github.com>
2020-02-22 00:09:03 +0500

    Connectivity

---
+++ MS-Sql Connectivity-Syntax.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            //Syntax to connect MS Sql Server 2014 with C#
+            //Data Source=Servername; Initial Catalog=DBName; Integrated Security=True;
+
+            SqlConnection aa = new SqlConnection("Data Source=FAHAD-PC;Initial Catalog=InvoiceDb;Integrated Security=True");
+            aa.Open();
+            MessageBox.Show("DB started");
+            SqlDataAdapter com = new SqlDataAdapter("SELECT * FROM Customer", aa);
+            DataTable dtb = new DataTable();
+            com.Fill(dtb);
+            dataGridView1.DataSource = dtb;
+        }
+    }
+}
